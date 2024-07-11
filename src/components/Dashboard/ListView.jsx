8bc017a99e447a0abced5be79651a52bb21932e1
Sakshi <saks0705@gmail.com>
2024-07-11 16:17:39 +0530

    files updated

--- src/components/Dashboard/ListView.jsx
+++ src/components/Dashboard/ListView.jsx
@@ -3,52 +3,57 @@ import { FaArrowTrendUp, FaArrowTrendDown } from "react-icons/fa6";
 
 const ListView = ({ coins }) => {
   return (
-    <div className='flex flex-col gap-5 p-1 md:p-4 py-10 w-[100%]  md:w-[90%] mx-auto'>
-      {coins.map((coin) => (
-        <div
-          key={coin.id}
-          className={`flex md:gap-[1rem] justify-between items-center py-4 px-2 md:p-4  rounded-lg dark:hover:bg-gray-950 hover:bg-gray-200 cursor-pointer transition duration-200 ${coin.price_change_percentage_24h > 0 ? "hover:border-green-500 hover:border-2" : "hover:border-red-500 hover:border-2"}`}
-        >
-          <div className='flex items-center'>
-            <img src={coin.image} alt="" className='h-[3rem] w-[3rem] rounded-full' />
-          </div>
-
-          <div>
-            <p className='text-gray-400 uppercase'>{coin.symbol}</p>
-            <p className='dark:text-white text-lg font-semibold'>{coin.name}</p>
-          </div>
-
-          {coin.price_change_percentage_24h > 0 ? (
-            <div className='flex items-center gap-5'>
-              <p className='text-green-500 border-2 border-green-500 px-5 py-2 text-lg font-semibold rounded-full hover:bg-green-500 hover:text-white'>+{coin.price_change_percentage_24h.toFixed(2)}%</p>
-              <p className='text-green-500 border-2 border-green-500 p-3 text-lg font-semibold rounded-full hover:bg-green-500 hover:text-white hidden md:block'><FaArrowTrendUp /></p>
+    <div className='w-[] md:w-[90%] mx-auto flex flex-col gap-5 py-10'>
+      {coins.map((coin) => {
+        return (
+          <div key={coin.id} className={`flex items-center justify-between  cursor-pointer dark:bg-gray-800 bg-gray-300 px-5 py-4 rounded-lg shadow-lg transform transition duration-500 hover:scale-105 hover:border-2 ${coin.price_change_percentage_24h > 0 ? "hover:border-green-500" : "hover:border-red-500"}`}>
+            {/* image */}
+            <div className='w-[2rem] lg:w-[3rem] lg:mr-[.5rem]'>
+              <img src={coin.image} alt="" className='w-[2rem] lg:w-[3rem] rounded-full' />
             </div>
-          ) : (
-            <div className='flex items-center gap-5'>
-              <p className='text-red-500 border-2 border-red-500 px-5 py-2 text-lg font-semibold rounded-full hover:bg-red-500 hover:text-white'>{coin.price_change_percentage_24h.toFixed(2)}%</p>
-              <p className='text-red-500 border-2 border-red-500 p-3 text-lg font-semibold rounded-full hover:bg-red-500 hover:text-white hidden md:block'><FaArrowTrendDown /></p>
+            {/* title and symbol */}
+            <div className='w-[10%]'>
+              <h1 className='text-gray-400 uppercase'>{coin.symbol}</h1>
+              <h1 className='dark:text-white text-sm lg:text-lg font-semibold'>{coin.name}</h1>
             </div>
-          )}
+            {/* extras */}
+            <div className='w-[18%] flex'>
+              {coin.price_change_percentage_24h > 0 ? (
+                <>
+                  <p className='text-green-500 border-2 border-green-500 px-5 py-2 text-lg font-semibold rounded-full hover:bg-green-500 hover:text-white'>+{coin.price_change_percentage_24h.toFixed(2)}%</p>
+                  <p className='text-green-500 border-2 border-green-500 p-3 text-lg font-semibold rounded-full hover:bg-green-500 hover:text-white hidden md:block'><FaArrowTrendUp /></p>
+                </>
 
-          {coin.price_change_percentage_24h > 0 ? (
-            <p className='text-lg font-bold text-green-500'>${coin.current_price}</p>
-          ) : (
-            <p className='text-lg font-bold text-red-500'>${coin.current_price}</p>
-          )}
+              ) : (
+                <>
+                  <p className='text-red-500 border-2 border-red-500 px-5 py-2 text-lg font-semibold rounded-full hover:bg-red-500 hover:text-white'>+{coin.price_change_percentage_24h.toFixed(2)}%</p>
+                  <p className='text-red-500 border-2 border-red-500 p-3 text-lg font-semibold rounded-full hover:bg-red-500 hover:text-white hidden md:block'><FaArrowTrendDown /></p>
+                </>
+              )}
 
-          <div>
-            <p className='text-lg text-black dark:text-white'>${coin.total_volume.toLocaleString()}</p>
+            </div>
+            <div className='w-[18%]'>
+              {coin.price_change_percentage_24h > 0 ? (
+                <p className='text-lg font-bold text-green-500'>${coin.current_price}</p>
+              ) : (
+                <p className='text-lg font-bold text-red-500'>${coin.current_price}</p>
+              )}
+
+            </div>
+            {/* total vol */}
+            <div className='w-[15%]'>
+              <p className='text-gray-500 text-[1rem]'>${coin.total_volume.toLocaleString()}</p>
+            </div>
+            {/* total cap */}
+            <div className='lg:w-[15%] hidden lg:block'>
+              <p className='text-gray-500 text-[1rem]'>${coin.market_cap.toLocaleString()}</p>
+            </div>
           </div>
+        )
 
-          <div>
-            <p className=' text-lg text-black dark:text-white hidden md:block'>${coin.market_cap.toLocaleString()}</p>
-          </div>
-
-        </div>
-      ))
-      }
-    </div >
-  )
+      })}
+    </div>
+  );
 }
 
 export default ListView;
