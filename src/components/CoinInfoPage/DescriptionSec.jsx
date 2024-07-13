92ed39eef0d177e04dbf0f56ff82a7a11652716f
Sakshi <saks0705@gmail.com>
2024-07-13 17:34:05 +0530

    files updated

--- src/components/CoinInfoPage/DescriptionSec.jsx
+++ src/components/CoinInfoPage/DescriptionSec.jsx
@@ -1,14 +1,25 @@
 import React, { useState } from 'react'
 
 const DescriptionSec = ({ desc }) => {
-    const [readMore,setReadMore]=useState(false);
-    
+    const [readMore, setReadMore] = useState(false);
+    const [editedDesc, setEditedDesc] = useState(desc.substring(0, 1000));
+
+    const handleReadMoreBtn = () => {
+        setReadMore(!readMore);
+        setEditedDesc(readMore ? desc.substring(0, 1000) : desc);
+    }
+
     return (
         <div className={`flex flex-col justify-between gap-3 cursor-pointer dark:bg-gray-800 bg-gray-300 dark:text-white px-5 py-4 rounded-lg shadow-lg`}>
             <h1 className='text-lg font-semibold'>Coin Description</h1>
-            <p>{desc}</p>
-            <button className='w-[10%] text-lg text-gray-400'>]
-            {readMore ? "Read less..." : "Read more..."}
+            <p className='text-justify'>
+                {editedDesc}
+                {!readMore && <span className='text-lg text-gray-400'>...</span>}
+            </p>
+            <button
+                onClick={handleReadMoreBtn}
+                className='text-lg text-gray-400'>
+                {readMore ? "Read less..." : "Read more..."}
             </button>
         </div>
     )
