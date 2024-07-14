ce8ce7b81b493e2984299f052750331cdf15fa3e
Sakshi <saks0705@gmail.com>
2024-07-14 09:37:23 +0530

    files updated

--- src/components/CoinInfoPage/DescriptionSec.jsx
+++ src/components/CoinInfoPage/DescriptionSec.jsx
@@ -1,4 +1,5 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
+import './Description.css';
 
 const DescriptionSec = ({ desc }) => {
     const [readMore, setReadMore] = useState(false);
@@ -7,22 +8,23 @@ const DescriptionSec = ({ desc }) => {
     const handleReadMoreBtn = () => {
         setReadMore(!readMore);
         setEditedDesc(readMore ? desc.substring(0, 1000) : desc);
-    }
+    };
 
     return (
-        <div className={`flex flex-col justify-between gap-3 cursor-pointer dark:bg-gray-800 bg-gray-300 dark:text-white px-5 py-4 rounded-lg shadow-lg`}>
-            <h1 className='text-lg font-semibold'>Coin Description</h1>
-            <p className='text-justify'>
-                {editedDesc}
-                {!readMore && <span className='text-lg text-gray-400'>...</span>}
-            </p>
+        <div className="flex flex-col justify-between gap-3 cursor-pointer dark:bg-gray-800 bg-gray-300 dark:text-white px-5 py-4 rounded-lg shadow-lg">
+            <h1 className="text-lg font-semibold">Coin Description</h1>
+            <div className="text-justify description">
+                <p dangerouslySetInnerHTML={{ __html: editedDesc }} />
+                {!readMore && <span className="text-lg text-gray-400">...</span>}
+            </div>
             <button
                 onClick={handleReadMoreBtn}
-                className='text-lg text-gray-400'>
-                {readMore ? "Read less..." : "Read more..."}
+                className="text-lg text-gray-400"
+            >
+                {readMore ? 'Read less...' : 'Read more...'}
             </button>
         </div>
-    )
-}
+    );
+};
 
-export default DescriptionSec
+export default DescriptionSec;
