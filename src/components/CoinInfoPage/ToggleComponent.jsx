e39d9a0032d3d1f69b6c136ebaa6740cef3b8bd7
Sakshi <saks0705@gmail.com>
2024-07-15 23:24:52 +0530

    files updated

--- src/components/CoinInfoPage/ToggleComponent.jsx
+++ src/components/CoinInfoPage/ToggleComponent.jsx
@@ -1,13 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react';
 
 const ToggleComponent = () => {
-    return (
-        <div className='flex md:items-center justify-start md:justify-center gap-1'>
-            <button className='text-[#7a4191] bg-[#7a4191] bg-opacity-15 border border-[#7a4191] px-3 py-2 rounded-sm text-lg'>Price</button>
-            <button className='text-[#7a4191] bg-[#7a4191] bg-opacity-15 border border-[#7a4191] px-3 py-2 rounded-sm text-lg'>MKT CAP</button>
-            <button className='text-[#7a4191] bg-[#7a4191] bg-opacity-15 border border-[#7a4191] px-3 py-2 rounded-sm text-lg'>VOLUME</button>
-        </div>
-    )
-}
+  const [priceType, setPriceType] = useState("prices");
 
-export default ToggleComponent
+  const handlePriceTypeChange = (newType) => {
+    setPriceType(newType);
+  };
+
+  return (
+    <div className="flex md:items-center justify-start md:justify-center gap-1">
+      <button
+        className={`text-[#7a4191] ${priceType === 'prices' ? 'bg-[#7a4191] bg-opacity-15' : 'bg-transparent'} border border-[#7a4191] px-3 py-2 rounded-sm text-lg`}
+        onClick={() => handlePriceTypeChange("prices")}
+      >
+        Price
+      </button>
+      <button
+        className={`text-[#7a4191] ${priceType === 'mktCap' ? 'bg-[#7a4191] bg-opacity-15' : 'bg-transparent'} border border-[#7a4191] px-3 py-2 rounded-sm text-lg`}
+        onClick={() => handlePriceTypeChange("mktCap")}
+      >
+        MKT CAP
+      </button>
+      <button
+        className={`text-[#7a4191] ${priceType === 'volume' ? 'bg-[#7a4191] bg-opacity-15' : 'bg-transparent'} border border-[#7a4191] px-3 py-2 rounded-sm text-lg`}
+        onClick={() => handlePriceTypeChange("volume")}
+      >
+        VOLUME
+      </button>
+    </div>
+  );
+};
+
+export default ToggleComponent;
--- src/functions/getCoinPrices.js
+++ src/functions/getCoinPrices.js
@@ -2,6 +2,8 @@ import axios from "axios";
 export const getCoinPrices = async (id,days) => {
     try {
         const { data } = await axios.get(`https://api.coingecko.com/api/v3/coins/${id}/market_chart?vs_currency=usd&days=${days}&interval=daily`);
+        console.log(data.market_caps);
+        console.log(data.total_volumes);
         return data.prices;
     } catch (error) {
         console.error("Error fetching coin prices:", error);
