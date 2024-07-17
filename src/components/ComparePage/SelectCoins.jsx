aa5b5ec09e118af56132aced8992de5aafb25cef
Sakshi <saks0705@gmail.com>
2024-07-17 18:42:45 +0530

    files updated

--- src/components/ComparePage/SelectCoins.jsx
+++ src/components/ComparePage/SelectCoins.jsx
@@ -3,21 +3,26 @@ import React, { useState } from 'react'
 const SelectCoins = ({ coins }) => {
     const [crypto1, setCrypto1] = useState("bitcoin");
     const [crypto2, setCrypto2] = useState("ethereum");
-    const handleCoinChange = (e) => {
-        setCrypto1(e.target.value);
-        console.log(e.target.value);
+    const handleCoinChange = (e, isCoin2) => {
+        if (isCoin2) {
+            setCrypto2(e.target.value);
+        }
+        else {
+            setCrypto1(e.target.value);
+        }
+        // console.log(e.target.value);/
     }
     return (
-        <div className="flex items-center cursor-pointer dark:bg-gray-800 bg-gray-300 px-5 py-4 rounded-lg shadow-lg transform transition duration-500 gap-1">
-            <div className='flex items-center gap-5'>
-                <p className='text-black dark:text-white'>Crypto 1</p>
+        <div className="flex cursor-pointer dark:bg-gray-800 bg-gray-300 px-5 py-4 rounded-lg shadow-lg transform transition duration-500 gap-1">
+            <div className='flex flex-row items-center gap-3 md:w-[20%]'>
+                <p className='text-black dark:text-white hidden md:block'>Crypto 1</p>
                 <select
                     name="crypto1"
                     id="crypto1"
                     label="Crypto 1"
                     value={crypto1}
-                    onChange={handleCoinChange}
-                    className="bg-white dark:bg-gray-700 border border-gray-300 dark:border-gray-600 rounded-md py-2 px-3 text-gray-900 dark:text-gray-100 focus:ring-2 focus:ring-purple-500 focus:border-purple-500 w-[40%]"
+                    onChange={(e) => handleCoinChange(e, false)}
+                    className="bg-white dark:bg-gray-700 border border-gray-300 dark:border-gray-600 rounded-md py-2 px-3 text-gray-900 dark:text-gray-100 focus:ring-2 focus:ring-purple-500 focus:border-purple-500 w-1/2"
                 >
                     {coins.map((coin) => (
                         <option key={coin.id} value={coin.id} className="text-gray-900 dark:text-gray-100">
@@ -28,15 +33,15 @@ const SelectCoins = ({ coins }) => {
 
                 </select>
             </div>
-            <div className='flex items-center gap-5'>
-                <p className='text-black dark:text-white'>Crypto 2</p>
+            <div className='flex flex-row items-center gap-3 md:w-[20%]'>
+                <p className='text-black dark:text-white hidden md:block'>Crypto 2</p>
                 <select
                     name="crypto1"
                     id="crypto1"
                     label="Crypto 1"
-                    value={crypto1}
-                    onChange={handleCoinChange}
-                    className="bg-white dark:bg-gray-700 border border-gray-300 dark:border-gray-600 rounded-md py-2 px-3 text-gray-900 dark:text-gray-100 focus:ring-2 focus:ring-purple-500 focus:border-purple-500 w-[40%]"
+                    value={crypto2}
+                    onChange={(e) => handleCoinChange(e, true)}
+                    className="bg-white dark:bg-gray-700 border border-gray-300 dark:border-gray-600 rounded-md py-2 px-3 text-gray-900 dark:text-gray-100 focus:ring-2 focus:ring-purple-500 focus:border-purple-500 w-1/2"
                 >
                     {coins.map((coin) => (
                         <option key={coin.id} value={coin.id} className="text-gray-900 dark:text-gray-100">
@@ -47,7 +52,6 @@ const SelectCoins = ({ coins }) => {
 
                 </select>
             </div>
-
         </div>
     )
 }
--- src/pages/Compare.jsx
+++ src/pages/Compare.jsx
@@ -1,11 +1,12 @@
 import React, { useEffect, useState } from 'react'
 import SelectCoins from '../components/ComparePage/SelectCoins'
 import get100Coins from '../functions/get100Coins';
+import SelectDays from '../components/CoinInfoPage/SelectDays'
 
 const Compare = () => {
   const [coins, setCoins] = useState([]);
-  const [loading,setLoading]=useState(true)
-  
+  const [loading, setLoading] = useState(true)
+
   const fetchCoins = async () => {
     try {
       const myCoins = await get100Coins();
@@ -18,15 +19,14 @@ const Compare = () => {
     }
   }
 
-
-
   useEffect(() => {
     fetchCoins();
-  },[])
+  }, [])
 
   return (
-    <div className='w-[97%] md:w-[94%] mx-auto flex flex-col gap-5 py-5 px-1'>
-      <SelectCoins coins={coins}/>
+    <div className='w-[97%] md:w-[94%] mx-auto flex flex-col py-5 px-1'>
+      <SelectCoins coins={coins} />
+      {/* <SelectDays /> */}
     </div>
   )
 }
